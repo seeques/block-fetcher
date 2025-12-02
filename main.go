9ef9bffc7fefb530a8dbf71b2079bd2089e4f5f7
seeques <equestrianbuddy@gmail.com>
2025-12-02 14:09:19 +0300

    Fetch block and account data

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"context"
 
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 func main() {
@@ -17,4 +19,21 @@ func main() {
 	fmt.Println("We have a connection!")
 
 	defer client.Close()
+
+	// Fetch the latest block
+	block, err := client.BlockByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Latest block number is %d\n", block.Number().Uint64())
+
+	// First account from anvil
+	account := common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Raw WEI balance is %s\n", balance)
 }
