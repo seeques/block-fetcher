d71a9d339a39a149785fe9e4cd3aba2873264835
seeques <equestrianbuddy@gmail.com>
2025-12-02 16:55:22 +0300

    Range over block and print value/gas in ETH

--- main.go
+++ main.go
@@ -23,6 +23,21 @@ func main() {
 
 	defer client.Close()
 
+	// First account from anvil
+	account := common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Raw WEI balance is %s\n", balance)
+
+	fbalance := new(big.Float)
+	fbalance.SetString(balance.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+
+	fmt.Printf("ETH balance is %f\n", ethValue)
+
 	// Fetch the latest block
 	block, err := client.BlockByNumber(context.Background(), nil)
 	if err != nil {
@@ -39,18 +54,13 @@ func main() {
 	formatted := t.Format("02 Jan 2006 15:04:05")
 	fmt.Printf("Block creation time is %s\n", formatted)
 
-	// First account from anvil
-	account := common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
-	balance, err := client.BalanceAt(context.Background(), account, nil)
-	if err != nil {
-		log.Fatal(err)
+	// Fetch all txs in a block
+	for _, tx := range block.Transactions() {
+		fmt.Printf("Tx hash is: %s\n", tx.Hash().Hex())
+
+		txValuef := new(big.Float).SetInt(tx.Value())
+		gasPricef := new(big.Float).SetInt(tx.GasPrice())
+		fmt.Printf("Tx value in ETH is: %s\n", new(big.Float).Quo(txValuef, big.NewFloat(math.Pow10(18))).Text('f', 18))
+		fmt.Printf("Tx gas price in ETH is: %s\n", new(big.Float).Quo(gasPricef, big.NewFloat(math.Pow10(18))).Text('f', 18))
 	}
-
-	fmt.Printf("Raw WEI balance is %s\n", balance)
-
-	fbalance := new(big.Float)
-	fbalance.SetString(balance.String())
-	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
-
-	fmt.Printf("ETH balance is %f\n", ethValue)
 }
