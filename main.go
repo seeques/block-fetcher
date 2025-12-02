1c01b706169b4d9c7688394c598a8f0ed794a790
seeques <equestrianbuddy@gmail.com>
2025-12-02 16:17:28 +0300

    Add timestamp conversion

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"math/big"
 	"math"
+	"time"
 
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/common"
@@ -29,6 +30,14 @@ func main() {
 	}
 
 	fmt.Printf("Latest block number is %d\n", block.Number().Uint64())
+
+	// Let's fetch the unix time and convert it to a more readable format
+	timestamp := block.Time()
+	fmt.Printf("Block timestamp is %d\n", timestamp)
+
+	t := time.Unix(int64(timestamp), 0)
+	formatted := t.Format("02 Jan 2006 15:04:05")
+	fmt.Printf("Block creation time is %s\n", formatted)
 
 	// First account from anvil
 	account := common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
